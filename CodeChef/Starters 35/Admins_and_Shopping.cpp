647bfca6baefb3563b6a7c01c8909a114e49a244
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-04-21 11:36:15 +0530

    Practice

--- CodeChef/Starters 35/Admins_and_Shopping.cpp
+++ CodeChef/Starters 35/Admins_and_Shopping.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
+#include<climits>
+
+long long int ceils(long long  int a, long long int b){
+        int z = a%b;
+        return (a%b==0 ? a/b : a/b+1);
+}
 
 int main(){
+    
+    // typedef ll long long;
+
     int t;
     cin>>t;
 
@@ -9,9 +18,17 @@ int main(){
         long long int n, x;
         cin>>n>>x;
         long long int arr[n];
+        long long int mn=INT_MAX;
         for(int i=0;i<n;i++){
             cin>>*(arr+i);
         }
+
+        for(int i=0;i<n;i++)
+            mn=min(mn, *(arr+i));
+
+        long long int ans = ceils(x, mn);
+
+        cout<<max(ans, n)<<"\n";
 
     }
     return 0;
